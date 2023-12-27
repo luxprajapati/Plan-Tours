72afb48f90eb915894a7d2720734f550ccfd9c49
Lux <luxprajapati81@gmail.com>
2023-12-27 17:05:59 +0530

    Plan Tours with LP

---
+++ src/components/Tours.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+
+function Tours({ tours, removeTour }) {
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title">Plan with Lux</h2>
+      </div>
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card {...tour} key={tour.id} removeTour={removeTour}></Card>;
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Tours;
